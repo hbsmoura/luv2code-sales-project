3d5a8d7f760f6fe464d652b198c3bcdccbb1756b
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-06-19 14:35:18 -0300

    implement sales person list component

--- src/app/components/sales-person-list/sales-person-list.component.ts
+++ src/app/components/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent {
+
+    salesPersonList: SalesPerson[] = [
+        new SalesPerson('Mano', 'Brown', 'manobrown@luv2code.com', 50000),
+        new SalesPerson('KL', 'Jay', 'kljay@luv2code.com', 40000),
+        new SalesPerson('Ice', 'Blue', 'iceblue@luv2code.com', 90000),
+        new SalesPerson('Edi', 'Rock', 'edirock@luv2code.com', 60000)
+    ]
 
 }
